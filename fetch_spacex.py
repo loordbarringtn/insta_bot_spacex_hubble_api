7c6cd69e7aea845a09a71c485e87701f37ff1b64
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2019-11-14 01:58:07 +0300

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,36 +5,29 @@ SPACEX_URL = "https://api.spacexdata.com/v3/launches/latest"
 IMAGE_DIRECTORY= 'images/'
 
 def spacex_save_file(content,IMAGE_DIRECTORY):
-    try:
-      if not os.path.exists(IMAGE_DIRECTORY):
-         os.mkdir(IMAGE_DIRECTORY)
-    except OSError:
-        print("Creation of the directory %s failed")
+    os.makedirs(IMAGE_DIRECTORY,exist_ok=True)
     filename = content.split("/")[-1]
     complete_filename = os.path.join(IMAGE_DIRECTORY, filename)
-    try:
-       response = requests.get(content)
-       response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-       print (err)
-    try:
-       with open(complete_filename, 'wb') as f:
+    response = requests.get(content)
+    response.raise_for_status()
+    with open(complete_filename, 'wb') as f:
           f.write(response.content)
           f.close()
-    except IOError as e:
-        print("I/O error({0}): {1}".format(e.errno, e.strerror))
 
 def fetch_spacex_last_launch(SPACEX_URL):
-    try:
-       response = requests.get(SPACEX_URL)
-       response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-       print (err)
+    response = requests.get(SPACEX_URL)
+    response.raise_for_status()
     data=response.json()
     photo_filename=data['links']['flickr_images']
     for i,b in enumerate (photo_filename):
         spacex_save_file(b,IMAGE_DIRECTORY)
 
 if __name__ == '__main__':
-    fetch_spacex_last_launch(SPACEX_URL)
-
+    try:
+      fetch_spacex_last_launch(SPACEX_URL)
+    except requests.exceptions.HTTPError as err:
+      print(err)
+    except OSError as error:
+      print("OS error: {0}".format(error))
+    except IOError as e:
+      print("I/O error({0}): {1}".format(e.errno, e.strerror))
