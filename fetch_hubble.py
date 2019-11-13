96ec3af394a76281ad0cafb1b22276c01f3ed8d9
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2019-11-14 01:58:46 +0300

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -5,46 +5,30 @@ IMAGE_COLLECTION='spacecraft'
 IMAGE_DIRECTORY= 'images/'
 
 def getting_image_url_from_image_id(image_id):
-    try:
-       response = requests.get(f'http://hubblesite.org/api/v3/image/{image_id}', verify=False)
-       response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-       print (err)
+    response = requests.get(f'http://hubblesite.org/api/v3/image/{image_id}', verify=False)
+    response.raise_for_status()
     data = response.json()
-    select_from_dictionary = data["image_files"]
-    specific_photo=select_from_dictionary[-1]
-    image_adress=specific_photo['file_url']
-    return image_adress
+    data_from_dictionary = data["image_files"]
+    specific_photo=data_from_dictionary[-1]
+    image_url=specific_photo['file_url']
+    return image_url
 
-
-def make_file_extension(image_id):
+def getting_file_extension(image_id):
     extension_from_url_name =getting_image_url_from_image_id(image_id).split('.')[-1]
     return extension_from_url_name
 
-
-def hubble_save_file(image_id,IMAGE_DIRECTORY):
-    try:
-      if not os.path.exists(IMAGE_DIRECTORY):
-         os.mkdir(IMAGE_DIRECTORY)
-    except OSError:
-        print("Creation of the directory %s failed")
-    image_full_web_adress = 'https:'+ getting_image_url_from_image_id(image_id)
-    file_name='hubble' + str(image_id)+'.'+make_file_extension(image_id)
+def save_hubble_images(image_id,IMAGE_DIRECTORY):
+    os.makedirs(IMAGE_DIRECTORY,exist_ok=True)
+    image_full_web_adress = '{}'.format('https:') + getting_image_url_from_image_id(image_id)
+    file_name = '{}{}{}'.format('hubble', image_id, '.') + getting_file_extension(image_id)
     complete_filename = os.path.join(IMAGE_DIRECTORY, file_name)
-    try:
-       response = requests.get(image_full_web_adress, verify=False)
-       response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-       print (err)
-    try:
-       with open(complete_filename, 'wb') as f:
+    response = requests.get(image_full_web_adress, verify=False)
+    response.raise_for_status()
+    with open(complete_filename, 'wb') as f:
           f.write(response.content)
           f.close()
-    except IOError as e:
-          print("I/O error({0}): {1}".format(e.errno, e.strerror))
 
-
-def hubble_collections_instant_download(IMAGE_COLLECTION,IMAGE_DIRECTORY):
+def download_hubble_collections(IMAGE_COLLECTION,IMAGE_DIRECTORY):
     url = f'http://hubblesite.org/api/v3/images?page=all&collection_name={IMAGE_COLLECTION}'
     response = requests.get(url)
     response.raise_for_status()
@@ -52,19 +36,19 @@ def hubble_collections_instant_download(IMAGE_COLLECTION,IMAGE_DIRECTORY):
     for spaceship in (response_data):
           image_full_web_adress = 'https:' + getting_image_url_from_image_id(spaceship['id'])
           file_name = image_full_web_adress.split("/")[-1]
-          print(file_name)
           complete_filename = os.path.join(IMAGE_DIRECTORY, file_name)
-          try:
-             response = requests.get(image_full_web_adress, verify=False)
-             response.raise_for_status()
-          except requests.exceptions.HTTPError as err:
-             print (err)
-          try:
-             with open(complete_filename, 'wb') as f:
+          response = requests.get(image_full_web_adress, verify=False)
+          response.raise_for_status()
+          with open(complete_filename, 'wb') as f:
                  f.write(response.content)
                  f.close()
-          except IOError as e:
-              print("I/O error({0}): {1}".format(e.errno, e.strerror))
 
 if __name__ == '__main__':
-    hubble_collections_instant_download(IMAGE_COLLECTION,IMAGE_DIRECTORY)
+    try:
+       download_hubble_collections(IMAGE_COLLECTION,IMAGE_DIRECTORY)
+    except requests.exceptions.HTTPError as err:
+       print(err)
+    except IOError as e:
+       print("I/O error({0}): {1}".format(e.errno, e.strerror))
+    except OSError as error:
+       print("OS error: {0}".format(error))
