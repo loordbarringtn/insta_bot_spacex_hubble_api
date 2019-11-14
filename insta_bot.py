e2f5ece90b4035ee764fad6f07bb65165d514935
loordbarringtn <55858015+loordbarringtn@users.noreply.github.com>
2019-11-15 01:12:34 +0300

    Update insta_bot.py

--- insta_bot.py
+++ insta_bot.py
@@ -4,16 +4,15 @@ import os
 from dotenv import load_dotenv
 PROCESSED_IMAGES='processed_images/'
 
-def insta_bot():
+def insta_bot(PROCESSED_IMAGES):
     load_dotenv()
     bot = Bot()
     instagram_username= os.getenv("username")
     instagram_password= os.getenv("password")
     bot.login(username=instagram_username, password=instagram_password)
-    files=os.listdir(PROCESSED_IMAGES)
-    for file in files:
+    for file in os.listdir(PROCESSED_IMAGES):
         caption=file.split('.')[0]
         bot.upload_photo(os.path.join(PROCESSED_IMAGES,file),caption=caption)
 
 if __name__ == '__main__':
-    insta_bot()
+    insta_bot(PROCESSED_IMAGES)
